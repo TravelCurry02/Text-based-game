1030a559a6b83cd5b57e78d1a50a47169191243d
TravelCurry02 <aceguy02@gmail.com>
2025-01-17 12:01:54 -0700

    done

--- Main.cs
+++ Main.cs
@@ -1,1 +1,30 @@
+using System;
+
+bool isRunning = true;
+while (isRunning)
+{
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine("1. Explore");
+    Console.WriteLine("2. Check Status");
+    Console.WriteLine("3. Quit");
+    Console.Write("Choose an option: ");
+    string input = Console.ReadLine();
+
+    switch (input)
+    {
+        case "1":
+            Console.WriteLine("You explore the world...");
+            break;
+        case "2":
+            Console.WriteLine("Your health is 100. Your score is 0.");
+            break;
+        case "3":
+            Console.WriteLine("Goodbye!");
+            isRunning = false;
+            break;
+        default:
+            Console.WriteLine("Invalid option. Try again.");
+            break;
+    }
+}
 
