40f83d4562344af4b3c0425d91fb830a389e76ff
erewegoagain <mcdougalpenguin@byui.edu>
2025-01-20 11:24:59 -0700

    Some code to start setting up the rooms and various movement.

--- TextedBasedGame/Program.cs
+++ TextedBasedGame/Program.cs
@@ -20,7 +20,9 @@ while (isRunning)
             Console.WriteLine("Your health is 100. Your score is 0.");
             break;
         case "3":
+            Console.WriteLine("Where do you want to go?");
             Console.WriteLine(" ");
+            string move = Console.ReadLine();
             break;
         case "4":
             Console.WriteLine("Goodbye!");
